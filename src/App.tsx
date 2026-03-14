5ea73a8670066cd4f8cd0ae560b97c0e2571e464
camila24513 <camila24513@cbtis75.edu.mx>
2026-03-13 22:27:25 -0600

    Avances pantalla Mi Alacena y configuración Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Inventory from './pages/Inventory/Inventory';
 
+
 function App() {
   return (
     <Inventory />
   )
 }
+
 
 export default App
--- src/pages/Inventory/Inventory.tsx
+++ src/pages/Inventory/Inventory.tsx
@@ -2,39 +2,58 @@ import React from 'react';
 //import IngredientForm from '../../components/IngredientForm/IngredientForm';
 
 const Inventory = () => {
-  return (
-    <div className="inventory-container">
+return (
+  <div className="min-h-screen bg-gray-100 p-10">
 
-      <h1>Mi Alacena</h1>
+    <h1 className="text-6xl text-green-500 font-bold">
+  Mi Alacena
+</h1>
 
-      <input 
-        type="search" 
-        placeholder="Buscar ingredientes..."
-      />
+    <div className="grid grid-cols-2 gap-8">
 
-      <div className="ingredient-grid">
+      {/* Formulario agregar ingrediente */}
+      <div className="bg-green-800 text-white p-6 rounded-xl">
 
-        <div className="ingredient-card-static">
-          Leche - 1L
-        </div>
+        <h2 className="text-2xl mb-6">Agregar ingrediente</h2>
 
-        <div className="ingredient-card-static">
-          Huevos - 12 unidades
-        </div>
+        <input
+          className="w-full p-2 mb-3 text-black rounded"
+          placeholder="Nombre del ingrediente"
+        />
 
-        <div className="ingredient-card-static">
-          Arroz - 500g
-        </div>
+        <input
+          className="w-full p-2 mb-3 text-black rounded"
+          placeholder="Cantidad"
+        />
+
+        <select className="w-full p-2 mb-3 text-black rounded">
+          <option>gramos</option>
+          <option>piezas</option>
+          <option>litros</option>
+        </select>
+
+        <button className="bg-green-300 text-black px-4 py-2 rounded mt-3">
+          Agregar
+        </button>
 
       </div>
 
-      <h2>Agregar Nuevo Ingrediente</h2>
+      {/* Lista de ingredientes */}
+      <div className="bg-green-200 p-6 rounded-xl">
 
-      {/* Aquí aparecerá el formulario que hará Irán */}
-      {/* <IngredientForm /> */}
+        <h2 className="text-2xl mb-4">Lista de ingredientes</h2>
+
+        <ul className="space-y-2">
+          <li>🥦 Brócoli - 2 piezas</li>
+          <li>🍗 Pechuga de pollo - 1 kg</li>
+        </ul>
+
+      </div>
 
     </div>
-  );
+
+  </div>
+);
 };
 
 export default Inventory;
